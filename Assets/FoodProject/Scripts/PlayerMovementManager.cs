024345abe1afe0e8d9b1af1b8d3f75849b0fce52
efekantgn <efekantgn@gmail.com>
2024-11-28 15:28:51 +0300

    feat and refactor: Player Movement Actions added and player animations added.

--- Assets/FoodProject/Scripts/PlayerMovementManager.cs
+++ Assets/FoodProject/Scripts/PlayerMovementManager.cs
@@ -9,6 +9,9 @@ using static UnityEngine.InputSystem.InputAction;
 
 public class PlayerMovementManager : MonoBehaviour
 {
+    private readonly int WalkingState = Animator.StringToHash("Armature|walking");
+    private readonly int IdleState = Animator.StringToHash("Armature|idle");
+    private Animator animator;
     public float MovementSpeed = 5f;
     public float Rotationspeed = 5f;
     private Rigidbody rb;
@@ -17,6 +20,9 @@ public class PlayerMovementManager : MonoBehaviour
     private bool isMoving = false;
     Vector3 moveDirection = Vector3.zero;
     Transform cameraTransform;
+    private float animatorStartSpeed;
+    public event Action OnPlayerMove;
+    public event Action OnPlayerStop;
 
     public bool IsMoving
     {
@@ -32,32 +38,53 @@ public class PlayerMovementManager : MonoBehaviour
     {
         inputManager = GetComponentInParent<PlayerInputManager>();
         rb = GetComponentInChildren<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
         cameraTransform = Camera.main.transform;
+    }
+    private void Start()
+    {
+        animatorStartSpeed = animator.speed;
     }
 
     private void OnEnable()
     {
         inputManager.OnMovePerform.AddListener(MovePerform);
         inputManager.OnMoveEnd.AddListener(MoveEnd);
+        OnPlayerMove += PlayerMoveStart;
+        OnPlayerStop += PlayerMoveStop;
     }
 
     private void OnDisable()
     {
         inputManager.OnMovePerform.RemoveListener(MovePerform);
         inputManager.OnMoveEnd.RemoveListener(MoveEnd);
+        OnPlayerMove -= PlayerMoveStart;
+        OnPlayerStop -= PlayerMoveStop;
     }
 
     private void MovePerform(Vector2 value)
     {
         //Debug.Log($"Move Perform at: {value}");
         moveInput = value;
-        IsMoving = true;
+        OnPlayerMove?.Invoke();
     }
 
     private void MoveEnd(Vector2 value)
     {
         //Debug.Log($"Move Ended at: {value}");
         moveInput = value;
+    }
+    private void PlayerMoveStart()
+    {
+        IsMoving = true;
+        animator.Play(WalkingState);
+        animator.speed = MovementSpeed;
+    }
+    private void PlayerMoveStop()
+    {
+        IsMoving = false;
+        animator.Play(IdleState);
+        animator.speed = animatorStartSpeed;
     }
 
     private void FixedUpdate()
@@ -80,7 +107,8 @@ public class PlayerMovementManager : MonoBehaviour
         }
         rb.velocity = moveDirection.normalized * MovementSpeed;
 
-        if (rb.velocity == Vector3.zero) IsMoving = false;
+        if (rb.velocity == Vector3.zero)
+            OnPlayerStop?.Invoke();
         // Karakteri hareket ettiği yöne döndür
         Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
         rb.MoveRotation(Quaternion.Slerp(rb.rotation, targetRotation, Time.fixedDeltaTime * Rotationspeed));
