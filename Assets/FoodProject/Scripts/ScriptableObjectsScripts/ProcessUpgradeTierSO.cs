c8b7d2cc0beca757f8da39f97e48b4f0ee98764f
efekantgn <efekantgn@gmail.com>
2024-12-05 16:01:59 +0300

    chore: Processor Upgrade Tier1 and tier10 created and menuname changed

---
+++ Assets/FoodProject/Scripts/ScriptableObjectsScripts/ProcessUpgradeTierSO.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewUpgradeTier", menuName = "FoodProject/Upgrades/New Processor Upgrade Tier", order = 0)]
+public class ProcessUpgradeTierSO : BaseUpgradeTierSO
+{
+    public float ProcessMultiplier;
+}
--- Assets/FoodProject/Scripts/ScriptableObjectsScripts/UpgradeTierSO.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewUpgradeTier", menuName = "FoodProject/Upgrades/New UpgradeTier", order = 0)]
-public class ProcessUpgradeTierSO : BaseUpgradeTierSO
-{
-    public float ProcessMultiplier;
-}
