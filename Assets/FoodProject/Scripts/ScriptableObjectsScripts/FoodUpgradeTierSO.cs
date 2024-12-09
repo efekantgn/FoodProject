26042fdacd293865df2154dcfc2b95a6caaa4f28
efekantgn <efekantgn@gmail.com>
2024-12-09 10:38:55 +0300

    new: Tiers created.

--- Assets/FoodProject/Scripts/ScriptableObjectsScripts/FoodUpgradeTierSO.cs
+++ Assets/FoodProject/Scripts/ScriptableObjectsScripts/FoodUpgradeTierSO.cs
@@ -3,5 +3,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Food Upgrade Tier", menuName = "FoodProject/Upgrades/New Food Upgrade Tier", order = 0)]
 public class FoodUpgradeTierSO : BaseUpgradeTierSO
 {
-    public int PriceMultiplier;
+    public float PriceMultiplier = 1;
 }
